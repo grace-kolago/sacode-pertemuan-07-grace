e31f04f5a80b8987ea4ec428f02b32debd00c71b
Grace <gracekolago21@gmail.com>
2024-08-29 04:04:12 -0700

    komit ke 2

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// single line commet
+
+/*
+
+multiple line comment
+bisa tulis juga disini
+
+*/
+
+var namaDepan ="Grace"
+var namaBelakang ="Kolago"
+
+document
+.getElementById("nama")
+.innerHTML = namaDepan + namaBelakang;
